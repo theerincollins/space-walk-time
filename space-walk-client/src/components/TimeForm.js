e65cfa236bd11a4b855a5ea1557ffa60954c8631
Erin Collins <erincollins@Erins-MacBook-Pro.local>
2020-07-30 14:16:36 -0700

    Added some error handling as well as resolved key error from react

--- space-walk-client/src/components/TimeForm.js
+++ space-walk-client/src/components/TimeForm.js
@@ -21,9 +21,21 @@ export default class TimeForm extends Component {
       body: JSON.stringify({ id: this.state.selectedTime })
     };
 
-    const response = await fetch('/astronaut/time_slots', requestOptions);
-    const selectedTime = await response.json();
-    alert(`Space Walk Confirmed! ${selectedTime.id}`);
+    fetch('/astronaut/time_slots', requestOptions)
+      .then(async response => {
+        const data = await response.json();
+
+        if (!response.ok) {
+          const error = (data && data.message) || response.status;
+          return Promise.reject(error);
+        }
+
+        alert(`Space Walk Confirmed! ${data.id}`);
+      })
+      .catch(error => {
+        this.setState({ errorMessage: error.toString() });
+        console.error('We had a problem!', error);
+      })
   }
 
   render() {
@@ -31,7 +43,7 @@ export default class TimeForm extends Component {
       <form onSubmit={this.handleSubmit}>
         <label>When would you like space person to walk?
           <select value={this.state.selectedTime} onChange={this.handleChange}>
-            {this.props.availableTimes.map(timeSlot => <option value={timeSlot.id}>{timeSlot.start_time}</option>)}
+            {this.props.availableTimes.map(timeSlot => <option key={timeSlot.id} value={timeSlot.id}>{timeSlot.start_time}</option>)}
           </select>
         </label>
         <input type="submit" value="Walk On!" />
